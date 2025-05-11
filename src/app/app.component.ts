b809bdff090dfdcf2fe384fe1ed1a50d559d04d6
Jeremias Santacruz <jeremias.santacruz@proveedor.ues21.edu.ar>
2025-05-11 16:51:14 -0300

    Add base page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CrearClienteComponent} from './crear-cliente/crear-cliente.component';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {ClienteFormComponent} from './cliente-form/cliente-form.component';
+import {EstadisticaCardComponent} from './estadistica-card/estadistica-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CrearClienteComponent, MatTabGroup, MatTab, ClienteFormComponent, EstadisticaCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
